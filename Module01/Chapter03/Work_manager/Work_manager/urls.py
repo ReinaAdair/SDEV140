fd152ff449e3e2befc66d59f237c688f71ea51d9
ReinaAdair <111796513+ReinaAdair@users.noreply.github.com>
2022-10-05 19:39:51 -0400

    10/05/2022

--- Module01/Chapter03/Work_manager/Work_manager/urls.py
+++ Module01/Chapter03/Work_manager/Work_manager/urls.py
@@ -1,19 +1,12 @@
 # *** Deprecated, use below instead 10-2022 jtp *** from django.conf.urls import include, url
-from django.urls import include, re_path
-
 from django.contrib import admin
+from django.urls import path
+
 admin.autodiscover()
 
+from TasksManager.views.index import home
+
 urlpatterns = [
-    # Examples:
-    # *** url is deprecated as of 1.6. using re_path instead - 10-2022 jtp ***
-    # url(r'^$', 'Work_manager.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    #url(r'^admin/', include(admin.site.urls)),
-    re_path(r'^admin/', admin.site.urls),
-
-    # from page 25 in book
-    # re_path(r'^$', 'TasksManager.views.index.page'), 
-    # re_path(r'^index$', 'TasksManager.views.index.page') 
+    path('admin/', admin.site.urls),
+    path("", home, name="home")
 ]
